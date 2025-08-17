07ee3e8b1286717140ffdf8a46b498a90eb4f65d
Fenomen <144873199+Fenomen1986@users.noreply.github.com>
2025-08-17 22:36:21 +0500

    Force custom admin site1

--- controlcenter/main/urls.py
+++ controlcenter/main/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-# --- ИЗМЕНЕНИЕ ---
-from controlcenter.main.admin import site as main_admin_site
 
 urlpatterns = [
-    # Заменяем стандартный admin.site на наш
-    path('admin/', main_admin_site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('controlcenter.main.urls')),
 ]

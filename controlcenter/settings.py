233f41bf2e16fddc7a631ad3909d22c0429b5343
Fenomen <144873199+Fenomen1986@users.noreply.github.com>
2025-08-17 18:02:34 +0500

    Migrate to PostgreSQL database

--- controlcenter/settings.py
+++ controlcenter/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,10 +64,9 @@ WSGI_APPLICATION = 'controlcenter.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 # Начинаем с простой SQLite базы данных.
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=False)
+}
 }
 
 
